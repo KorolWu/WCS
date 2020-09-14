17a9541668acce48d541f7dc33d42d47a42f6c28
janwcs <ying4090105335@126.com>
2020-09-14 13:39:28 +0800

    仓库基本信息间距体调整

--- WarehouseControlSystem/Forms/mainFroms/warehouseprofileinfoshowwg.cpp
+++ WarehouseControlSystem/Forms/mainFroms/warehouseprofileinfoshowwg.cpp
@@ -3,15 +3,25 @@
 WarehouseprofileInfoShowWg::WarehouseprofileInfoShowWg(int w, int h, QWidget *parent): QWidget(parent)
 {
     this->resize(w,h);
+    m_size = -1;
+   m_address = "昆山";
+   m_type  ="标准件仓";
+   m_state = 1;
+}
 
-
+void WarehouseprofileInfoShowWg::SetParam(int size, QString address, QString type, int state)
+{
+    m_size = size;
+    m_address = address;
+    m_type = type;
+    m_state = state;
 }
 
 void WarehouseprofileInfoShowWg::paintEvent(QPaintEvent *e)
 {
     //绘制边框
     QPainter painter(this);
-    painter.setPen(QColor(139, 139, 139));
+    painter.setPen(QColor(200, 200, 200));
     painter.drawLine(0, 0, this->width() - 1, 0);
     painter.drawLine(0, 0, 0, this->height() - 1);
     painter.drawLine(this->width()-1 , 0, this->width() - 1, this->height()-1 );
@@ -23,17 +33,17 @@ void WarehouseprofileInfoShowWg::paintEvent(QPaintEvent *e)
     QFont font;
     font.setFamily("Microsoft YaHei");
     // 大小
-    font.setPointSize(16);
+    font.setPointSize(14);
     // 斜体
     // font.setItalic(true);
     // 设置下划线
     font.setUnderline(true);
     // 设置上划线
-    // font.setOverline(true);
+   //  font.setOverline(true);
     // 设置字母大小写
     font.setCapitalization(QFont::SmallCaps);
     // 设置字符间距
-    font.setLetterSpacing(QFont::AbsoluteSpacing, 5);
+    font.setLetterSpacing(QFont::AbsoluteSpacing, 3);
     //使用字体
     painter.setFont(font);
     //绘制区域从x坐标100，y坐标100处开始
@@ -41,17 +51,23 @@ void WarehouseprofileInfoShowWg::paintEvent(QPaintEvent *e)
     //绘制库位配置数据显示
     font.setPointSize(12);
     font.setUnderline(false);
-    painter.setPen(QColor(80, 80, 80,200));
+    painter.setPen(QColor(100, 100, 100,200));
     font.setLetterSpacing(QFont::AbsoluteSpacing, 0);
     painter.setFont(font);
-    painter.drawText(this->width()/4+10, this->height()/5 , QStringLiteral("总库位"));
-    painter.drawText(this->width()/4, this->height()/5*2 , QStringLiteral("仓库地点"));
-    painter.drawText(this->width()/4, this->height()/5*3 , QStringLiteral("仓库类型"));
-    painter.drawText(this->width()/4,  this->height()/5*4 , QStringLiteral("仓库状态"));
+    painter.drawText(this->width()/5+10, this->height()/5 , QStringLiteral("总库位"));
+    painter.drawText(this->width()/5, this->height()/5*2 , QStringLiteral("仓库地点"));
+    painter.drawText(this->width()/5, this->height()/5*3 , QStringLiteral("仓库类型"));
+    painter.drawText(this->width()/5,  this->height()/5*4 , QStringLiteral("仓库状态"));
     //仓位配置数据的显示
-     painter.setPen(QColor(0, 40, 90));
-    font.setPointSize(28);
-     painter.setFont(font);
-      painter.drawText(this->width()*2/3,  this->height()/5 , QStringLiteral("13490"));
+    painter.setPen(QColor(0, 70, 200));
+    font.setPointSize(23);
+    painter.setFont(font);
+    painter.drawText(this->width()*2/3,  this->height()/5 , QStringLiteral("13490"));
+    painter.drawText(this->width()*2/3,  this->height()/5*2 , QStringLiteral("昆山"));
+    painter.drawText(this->width()*2/3-30,  this->height()/5*3 , QStringLiteral("电控件仓"));
+    painter.setPen(QColor(0, 200, 0));
+    font.setPointSize(23);
+    painter.setFont(font);
+    painter.drawText(this->width()*2/3,  this->height()/5*4 , QStringLiteral("正常"));
 
 }
--- WarehouseControlSystem/Forms/mainFroms/warehouseprofileinfoshowwg.h
+++ WarehouseControlSystem/Forms/mainFroms/warehouseprofileinfoshowwg.h
@@ -8,11 +8,18 @@
 
 class WarehouseprofileInfoShowWg : public QWidget
 {
-        Q_OBJECT
+    Q_OBJECT
 public:
     WarehouseprofileInfoShowWg(int w, int h, QWidget *parent = 0);
+    void SetParam(int size,QString address,QString type,int state );
 protected:
     void paintEvent(QPaintEvent *e);
+private:
+    int m_size;
+    QString m_address;
+    QString m_type;
+    int m_state;
+
 };
 
 #endif // WAREHOUSEPROFILEINFOSHOWWG_H
